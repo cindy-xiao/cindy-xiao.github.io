ba6664aa9b79fb0e6b4db1808b3cd3bdb2373a64
Jingyan Xiao <icefairy279@hotmail.com>
2016-07-23 07:50:23 -0400

    plz work

--- javaScript.js
+++ javaScript.js
@@ -2,14 +2,14 @@
 $(document).ready(function() {
 	var scrollMagicController = new ScrollMagic.Controller();
 
-	var tween = TweenMax.to("div", 2.5, {
+	var tween = new TweenMax.to("#block", 2.5, {
 	backgroundColor: 'red',
 	color: "white"
 	    });
 	var scene = new ScrollMagic.Scene({
-		triggerElement: "div",
+		triggerElement: "container",
 	    })
 	    .setTween(tween)
-	    .addIndicators({name: "1 (duration: 0)"})
+	    .addIndicators()
 	    .addTo(scrollMagicController);
     });
