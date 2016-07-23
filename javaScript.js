53f5c06c62ec0ef8b9733b90079366a08ab65471
Jingyan Xiao <icefairy279@hotmail.com>
2016-07-23 07:56:41 -0400

    i dont udnerstand

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,17 @@
 
 $(document).ready(function() {
-	var scrollMagicController = new ScrollMagic.Controller();
+	$(function() {
+		var controller = new ScrollMagic.Controller();
 
-	var tween = new TweenMax.to("#block", 2.5, {
-	backgroundColor: 'red',
-	color: "white"
+		var blockTween = new TweenMax.to('#block', 1.5, {
+			backgroundColor: 'red'
+		    });
+
+		var containerScene = new ScrollMagic.Scene({
+			triggerElement: '#container'
+		    })
+		    .setTween(blockTween)
+		    .addIndicators()
+		    .addTo(controller);
 	    });
-	var scene = new ScrollMagic.Scene({
-		triggerElement: "container",
-	    })
-	    .setTween(tween)
-	    .addIndicators()
-	    .addTo(scrollMagicController);
     });
